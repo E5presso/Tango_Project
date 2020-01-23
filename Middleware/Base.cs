d54d0f80b587273584b16db3e025c49c62835cb3
백세종 <sejong418@icloud.com>
2020-01-23 16:22:17 +0900

    로봇제어 미들웨어 프레임워크 추가

---
+++ Middleware/Base.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Middleware
+{
+	public enum StatusCode
+	{
+		PASS,
+		FAILED,
+		FIRST_BENDED,
+		SECOND_BENDED
+	}
+	public enum PhaseCode
+	{
+		A_PHASE,
+		B_PHASE,
+		C_PHASE,
+		D_PHASE,
+		E_PHASE,
+		F_PHASE,
+		G_PHASE
+	}
+
+	public struct SensorValue
+	{
+		public float Sensor1 { get; private set; }
+		public float Sensor2 { get; private set; }
+
+		public SensorValue(float sensor1, float sensor2)
+		{
+			Sensor1 = sensor1;
+			Sensor2 = sensor2;
+		}
+	}
+
+	public class SensorValueEventArgs : EventArgs
+	{
+		public StatusCode Status { get; private set; }
+		public SensorValue Before { get; private set; }
+		public SensorValue? After { get; private set; }
+		public SensorValue? Delta { get; private set; }
+
+		public SensorValueEventArgs(StatusCode status, SensorValue before, SensorValue? after, SensorValue? delta)
+		{
+			Status = status;
+			Before = before;
+			After = after;
+			Delta = delta;
+		}
+	}
+	public class RobotPhaseEventArgs : EventArgs
+	{
+		public PhaseCode Phase;
+
+		public RobotPhaseEventArgs(PhaseCode phase)
+		{
+			Phase = phase;
+		}
+	}
+}
---
+++ Middleware/Sensor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Core.Network;
+
+namespace Middleware
+{
+	internal class Sensor
+	{
+		private readonly Client sensor1 = new Client();
+		private readonly Client sensor2 = new Client();
+
+		public event EventHandler<ConnectEventArgs> Sensor1Connected;
+		public event EventHandler<SendEventArgs> Sensor1Sended;
+		public event EventHandler<ReceiveEventArgs> Sensor1Received;
+		public event EventHandler<DisconnectEventArgs> Sensor1Disconnected;
+		public event EventHandler<ExceptionEventArgs> Sensor1ErrorOccurred;
+
+		public event EventHandler<ConnectEventArgs> Sensor2Connected;
+		public event EventHandler<SendEventArgs> Sensor2Sended;
+		public event EventHandler<ReceiveEventArgs> Sensor2Received;
+		public event EventHandler<DisconnectEventArgs> Sensor2Disconnected;
+		public event EventHandler<ExceptionEventArgs> Sensor2ErrorOccurred;
+
+		public bool IsSensor1Connected => sensor1.IsActive;
+		public bool IsSensor2Connected => sensor2.IsActive;
+
+		public Sensor()
+		{
+			sensor1.Connected += Sensor1_Connected;
+			sensor1.Sended += Sensor1_Sended;
+			sensor1.Received += Sensor1_Received;
+			sensor1.Disconnected += Sensor1_Disconnected;
+			sensor1.ErrorOccurred += Sensor1_ErrorOccurred;
+
+			sensor2.Connected += Sensor2_Connected;
+			sensor2.Sended += Sensor2_Sended;
+			sensor2.Received += Sensor2_Received;
+			sensor2.Disconnected += Sensor2_Disconnected;
+			sensor2.ErrorOccurred += Sensor2_ErrorOccurred;
+		}
+
+		public void ConnectToSensor1(string ip, int port, int buffersize) => sensor1.Open(ip, port, buffersize, false);
+		public void ConnectToSensor2(string ip, int port, int buffersize) => sensor2.Open(ip, port, buffersize, false);
+		public void DisconnectFromSensor1() => sensor1.Close();
+		public void DisconnectFromSensor2() => sensor2.Close();
+		public void SendToSensor1(byte[] data) => sensor1.Send(data);
+		public void SendToSensor2(byte[] data) => sensor2.Send(data);
+
+		private void Sensor1_Connected(object sender, ConnectEventArgs e) => Sensor1Connected?.Invoke(sender, e);
+		private void Sensor1_Sended(object sender, SendEventArgs e) => Sensor1Sended?.Invoke(sender, e);
+		private void Sensor1_Received(object sender, ReceiveEventArgs e) => Sensor1Received?.Invoke(sender, e);
+		private void Sensor1_Disconnected(object sender, DisconnectEventArgs e) => Sensor1Disconnected?.Invoke(sender, e);
+		private void Sensor1_ErrorOccurred(object sender, ExceptionEventArgs e) => Sensor1ErrorOccurred?.Invoke(sender, e);
+
+		private void Sensor2_Connected(object sender, ConnectEventArgs e) => Sensor2Connected?.Invoke(sender, e);
+		private void Sensor2_Sended(object sender, SendEventArgs e) => Sensor2Sended?.Invoke(sender, e);
+		private void Sensor2_Received(object sender, ReceiveEventArgs e) => Sensor2Received?.Invoke(sender, e);
+		private void Sensor2_Disconnected(object sender, DisconnectEventArgs e) => Sensor2Disconnected?.Invoke(sender, e);
+		private void Sensor2_ErrorOccurred(object sender, ExceptionEventArgs e) => Sensor2ErrorOccurred?.Invoke(sender, e);
+	}
+}
