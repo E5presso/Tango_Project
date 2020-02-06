49d3a3630d41648362b9bb83731c00c490838dd2
백세종 <sejong418@icloud.com>
2020-02-06 23:32:13 +0900

    Interlock 테스팅용 프로그램 추가

---
+++ InterlockTest/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace InterlockTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.Pass = new System.Windows.Forms.Button();
+			this.Ng = new System.Windows.Forms.Button();
+			this.BendingPass = new System.Windows.Forms.Button();
+			this.BendingNg = new System.Windows.Forms.Button();
+			this.PcStatus = new System.Windows.Forms.Button();
+			this.PcComError = new System.Windows.Forms.Button();
+			this.SensorStatus = new System.Windows.Forms.Button();
+			this.SensorDataStatus = new System.Windows.Forms.Button();
+			this.Bypass = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.Pass);
+			this.flowLayoutPanel1.Controls.Add(this.Ng);
+			this.flowLayoutPanel1.Controls.Add(this.BendingPass);
+			this.flowLayoutPanel1.Controls.Add(this.BendingNg);
+			this.flowLayoutPanel1.Controls.Add(this.PcStatus);
+			this.flowLayoutPanel1.Controls.Add(this.PcComError);
+			this.flowLayoutPanel1.Controls.Add(this.SensorStatus);
+			this.flowLayoutPanel1.Controls.Add(this.SensorDataStatus);
+			this.flowLayoutPanel1.Controls.Add(this.Bypass);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+			this.flowLayoutPanel1.TabIndex = 0;
+			// 
+			// Pass
+			// 
+			this.Pass.Enabled = false;
+			this.Pass.Location = new System.Drawing.Point(3, 3);
+			this.Pass.Name = "Pass";
+			this.Pass.Size = new System.Drawing.Size(260, 116);
+			this.Pass.TabIndex = 0;
+			this.Pass.Text = "PASS";
+			this.Pass.UseVisualStyleBackColor = true;
+			this.Pass.Click += new System.EventHandler(this.Pass_Click);
+			// 
+			// Ng
+			// 
+			this.Ng.Enabled = false;
+			this.Ng.Location = new System.Drawing.Point(269, 3);
+			this.Ng.Name = "Ng";
+			this.Ng.Size = new System.Drawing.Size(260, 116);
+			this.Ng.TabIndex = 1;
+			this.Ng.Text = "NG";
+			this.Ng.UseVisualStyleBackColor = true;
+			this.Ng.Click += new System.EventHandler(this.Ng_Click);
+			// 
+			// BendingPass
+			// 
+			this.BendingPass.Enabled = false;
+			this.BendingPass.Location = new System.Drawing.Point(535, 3);
+			this.BendingPass.Name = "BendingPass";
+			this.BendingPass.Size = new System.Drawing.Size(260, 116);
+			this.BendingPass.TabIndex = 2;
+			this.BendingPass.Text = "BENDING_PASS";
+			this.BendingPass.UseVisualStyleBackColor = true;
+			this.BendingPass.Click += new System.EventHandler(this.BendingPass_Click);
+			// 
+			// BendingNg
+			// 
+			this.BendingNg.Enabled = false;
+			this.BendingNg.Location = new System.Drawing.Point(3, 125);
+			this.BendingNg.Name = "BendingNg";
+			this.BendingNg.Size = new System.Drawing.Size(260, 116);
+			this.BendingNg.TabIndex = 3;
+			this.BendingNg.Text = "BENDING_NG";
+			this.BendingNg.UseVisualStyleBackColor = true;
+			this.BendingNg.Click += new System.EventHandler(this.BendingNg_Click);
+			// 
+			// PcStatus
+			// 
+			this.PcStatus.Location = new System.Drawing.Point(269, 125);
+			this.PcStatus.Name = "PcStatus";
+			this.PcStatus.Size = new System.Drawing.Size(260, 116);
+			this.PcStatus.TabIndex = 4;
+			this.PcStatus.Text = "PC_STATUS";
+			this.PcStatus.UseVisualStyleBackColor = true;
+			this.PcStatus.Click += new System.EventHandler(this.PcStatus_Click);
+			// 
+			// PcComError
+			// 
+			this.PcComError.Location = new System.Drawing.Point(535, 125);
+			this.PcComError.Name = "PcComError";
+			this.PcComError.Size = new System.Drawing.Size(260, 116);
+			this.PcComError.TabIndex = 5;
+			this.PcComError.Text = "PC_COM_ERROR";
+			this.PcComError.UseVisualStyleBackColor = true;
+			this.PcComError.Click += new System.EventHandler(this.PcComError_Click);
+			// 
+			// SensorStatus
+			// 
+			this.SensorStatus.Location = new System.Drawing.Point(3, 247);
+			this.SensorStatus.Name = "SensorStatus";
+			this.SensorStatus.Size = new System.Drawing.Size(260, 116);
+			this.SensorStatus.TabIndex = 6;
+			this.SensorStatus.Text = "SENSOR_STATUS";
+			this.SensorStatus.UseVisualStyleBackColor = true;
+			this.SensorStatus.Click += new System.EventHandler(this.SensorStatus_Click);
+			// 
+			// SensorDataStatus
+			// 
+			this.SensorDataStatus.Enabled = false;
+			this.SensorDataStatus.Location = new System.Drawing.Point(269, 247);
+			this.SensorDataStatus.Name = "SensorDataStatus";
+			this.SensorDataStatus.Size = new System.Drawing.Size(260, 116);
+			this.SensorDataStatus.TabIndex = 7;
+			this.SensorDataStatus.Text = "SENSOR_DATA_STATUS";
+			this.SensorDataStatus.UseVisualStyleBackColor = true;
+			this.SensorDataStatus.Click += new System.EventHandler(this.SensorDataStatus_Click);
+			// 
+			// Bypass
+			// 
+			this.Bypass.Location = new System.Drawing.Point(535, 247);
+			this.Bypass.Name = "Bypass";
+			this.Bypass.Size = new System.Drawing.Size(260, 116);
+			this.Bypass.TabIndex = 8;
+			this.Bypass.Text = "BYPASS";
+			this.Bypass.UseVisualStyleBackColor = true;
+			this.Bypass.Click += new System.EventHandler(this.Bypass_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Button Pass;
+		private System.Windows.Forms.Button Ng;
+		private System.Windows.Forms.Button BendingPass;
+		private System.Windows.Forms.Button BendingNg;
+		private System.Windows.Forms.Button PcStatus;
+		private System.Windows.Forms.Button PcComError;
+		private System.Windows.Forms.Button SensorStatus;
+		private System.Windows.Forms.Button SensorDataStatus;
+		private System.Windows.Forms.Button Bypass;
+	}
+}
+
---
+++ InterlockTest/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Core.Collections;
+using Core.Network;
+
+namespace InterlockTest
+{
+	public partial class Form1 : Form
+	{
+		private bool PcStatusFlag = true;
+		private bool SocketFlag = true;
+		private bool BypassFlag = false;
+		private bool SensorStatusFlag = false;
+		private int BendingSequence = 0;
+
+		private readonly Server server = new Server();
+		private readonly Thread thread;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			server.Connected += Server_Connected;
+			server.Sended += Server_Sended;
+			server.Received += Server_Received;
+			server.Disconnected += Server_Disconnected;
+			server.ErrorOccurred += Server_ErrorOccurred;
+
+			server.Open(11002, 100, 1500, false);
+			thread = new Thread(new ThreadStart(() =>
+			{
+				while (true)
+				{
+					if (PcStatusFlag)
+					{
+						string url1 = "http://124.127.248.84/KCLDO/SET%20VAR%20[REAL_OUT]pgs=true";
+						HttpWebRequest request1 = WebRequest.Create(url1) as HttpWebRequest;
+						request1.GetResponse();
+
+						string url2 = "http://124.127.248.85/KCLDO/SET%20VAR%20[REAL_OUT]pgs=true";
+						HttpWebRequest request2 = WebRequest.Create(url2) as HttpWebRequest;
+						request2.GetResponse();
+					}
+					Thread.Sleep(1000);
+				}
+			}));
+			thread.Start();
+		}
+
+		private void EnableButtons()
+		{
+			Pass.Enabled = true;
+			Ng.Enabled = true;
+			BendingPass.Enabled = true;
+			BendingNg.Enabled = true;
+			SensorDataStatus.Enabled = true;
+		}
+		private void DisableButtons()
+		{
+			Pass.Enabled = false;
+			Ng.Enabled = false;
+			BendingPass.Enabled = false;
+			BendingNg.Enabled = false;
+			SensorDataStatus.Enabled = false;
+		}
+
+		private void Server_Connected(object sender, ConnectEventArgs e)
+		{
+		}
+		private void Server_Sended(object sender, SendEventArgs e)
+		{
+		}
+		private void Server_Received(object sender, ReceiveEventArgs e)
+		{
+			byte[] packet = e.Data;
+			if (packet[4] == 0x31 && packet[5] == 0x31)
+			{
+				// Door Information
+				packet[5] = 0x32;
+				server.Send(packet);
+				EnableButtons();
+			}
+			else if (packet[4] == 0x32 && packet[5] == 0x34)
+			{
+				if (BendingSequence == 1)
+				{
+					packet = new byte[]
+					{
+						0x39, 0x37, 0x39, 0x30, 0x33, 0x35, 0x46, 0x46,
+						0x38, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30,
+						0x0D, 0x0A
+					};
+					server.Send(packet);
+					BendingSequence = 0;
+					DisableButtons();
+				}
+				else if (BendingSequence == 2)
+				{
+					packet = new byte[]
+					{
+						0x39, 0x37, 0x39, 0x30, 0x33, 0x35, 0x46, 0x46,
+						0x38, 0x33, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30,
+						0x0D, 0x0A
+					};
+					server.Send(packet);
+					BendingSequence = 0;
+					DisableButtons();
+				}
+				else if (BendingSequence == 3)
+				{
+					BendingSequence = 0;
+					DisableButtons();
+				}
+			}
+		}
+		private void Server_Disconnected(object sender, DisconnectEventArgs e)
+		{
+		}
+		private void Server_ErrorOccurred(object sender, ExceptionEventArgs e)
+		{
+		}
+
+		private void Pass_Click(object sender, EventArgs e)
+		{
+			byte[] packet = new byte[]
+			{
+				0x39, 0x37, 0x39, 0x30, 0x33, 0x35, 0x46, 0x46,
+				0x38, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30,
+				0x0D, 0x0A
+			};
+			server.Send(packet);
+			DisableButtons();
+		}
+		private void Ng_Click(object sender, EventArgs e)
+		{
+			byte[] packet = new byte[]
+			{
+				0x39, 0x37, 0x39, 0x30, 0x33, 0x35, 0x46, 0x46,
+				0x38, 0x33, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30,
+				0x0D, 0x0A
+			};
+			server.Send(packet);
+			DisableButtons();
+		}
+		private void BendingPass_Click(object sender, EventArgs e)
+		{
+			byte[] packet = new byte[]
+			{
+				0x39, 0x37, 0x39, 0x30, 0x32, 0x33, 0x30, 0x30,
+				0x38, 0x46, 0x30, 0x30, 0x2B, 0x30, 0x30, 0x30,
+				0x30, 0x30, 0x31, 0x30, 0x30, 0x2D, 0x30, 0x30,
+				0x30, 0x30, 0x30, 0x31, 0x30, 0x30, 0x0D, 0x0A
+			};
+			server.Send(packet);
+			BendingSequence = 1;
+		}
+		private void BendingNg_Click(object sender, EventArgs e)
+		{
+			byte[] packet = new byte[]
+			{
+				0x39, 0x37, 0x39, 0x30, 0x32, 0x33, 0x30, 0x30,
+				0x38, 0x46, 0x30, 0x30, 0x2B, 0x30, 0x30, 0x30,
+				0x30, 0x30, 0x31, 0x30, 0x30, 0x2D, 0x30, 0x30,
+				0x30, 0x30, 0x30, 0x31, 0x30, 0x30, 0x0D, 0x0A
+			};
+			server.Send(packet);
+			BendingSequence = 2;
+		}
+		private void PcStatus_Click(object sender, EventArgs e)
+		{
+			PcStatusFlag = !PcStatusFlag;
+		}
+		private void PcComError_Click(object sender, EventArgs e)
+		{
+			if (SocketFlag)
+			{
+				server.Close();
+				SocketFlag = false;
+			}
+			else
+			{
+				server.Open(11002, 100, 1500, false);
+				SocketFlag = true;
+			}
+		}
+		private void SensorStatus_Click(object sender, EventArgs e)
+		{
+			if (SensorStatusFlag)
+			{
+				string url1 = "http://124.127.248.84/KCLDO/SET%20PORT%20DOUT[647]=OFF";
+				HttpWebRequest request1 = WebRequest.Create(url1) as HttpWebRequest;
+				request1.GetResponse();
+
+				string url2 = "http://124.127.248.85/KCLDO/SET%20PORT%20DOUT[647]=OFF";
+				HttpWebRequest request2 = WebRequest.Create(url2) as HttpWebRequest;
+				request2.GetResponse();
+
+				SensorStatusFlag = false;
+			}
+			else
+			{
+				string url1 = "http://124.127.248.84/KCLDO/SET%20PORT%20DOUT[647]=ON";
+				HttpWebRequest request1 = WebRequest.Create(url1) as HttpWebRequest;
+				request1.GetResponse();
+
+				string url2 = "http://124.127.248.85/KCLDO/SET%20PORT%20DOUT[647]=ON";
+				HttpWebRequest request2 = WebRequest.Create(url2) as HttpWebRequest;
+				request2.GetResponse();
+
+				SensorStatusFlag = true;
+			}
+		}
+		private void SensorDataStatus_Click(object sender, EventArgs e)
+		{
+			byte[] packet = new byte[]
+			{
+				0x39, 0x37, 0x39, 0x30, 0x32, 0x33, 0x30, 0x30,
+				0x38, 0x46, 0x30, 0x30, 0x2B, 0x30, 0x30, 0x30,
+				0x30, 0x39, 0x30, 0x30, 0x30, 0x2D, 0x30, 0x30,
+				0x30, 0x30, 0x39, 0x30, 0x30, 0x30, 0x0D, 0x0A
+			};
+			server.Send(packet);
+			BendingSequence = 3;
+		}
+		private void Bypass_Click(object sender, EventArgs e)
+		{
+			if (BypassFlag)
+			{
+				string url1 = "http://124.127.248.84/KCLDO/SET%20PORT%20DOUT[649]=OFF";
+				HttpWebRequest request1 = WebRequest.Create(url1) as HttpWebRequest;
+				request1.GetResponse();
+
+				string url2 = "http://124.127.248.85/KCLDO/SET%20PORT%20DOUT[649]=OFF";
+				HttpWebRequest request2 = WebRequest.Create(url2) as HttpWebRequest;
+				request2.GetResponse();
+
+				BypassFlag = false;
+			}
+			else
+			{
+				string url1 = "http://124.127.248.84/KCLDO/SET%20PORT%20DOUT[649]=ON";
+				HttpWebRequest request1 = WebRequest.Create(url1) as HttpWebRequest;
+				request1.GetResponse();
+
+				string url2 = "http://124.127.248.85/KCLDO/SET%20PORT%20DOUT[649]=ON";
+				HttpWebRequest request2 = WebRequest.Create(url2) as HttpWebRequest;
+				request2.GetResponse();
+
+				BypassFlag = true;
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			thread.Abort();
+			server.Dispose();
+		}
+	}
+}
